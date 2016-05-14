658c74c4226b789158b61fbf0b1d72925e4fdf44
Cziczarito <Cziczarito@ICSTONT>
2016-05-14 14:43:18 +0200

    Preparing for db

--- Projekt/Projekt/main.cpp
+++ Projekt/Projekt/main.cpp
@@ -1,21 +1,26 @@
 #include "projekt.h"
+#include "Zwnswn.h"
 #include <QtWidgets/QApplication>
 #include <QDebug>
 #include <QDate>
 #include <cmath>
 using namespace std;
+
+QDebug operator<< (QDebug d, const Zwnswn &c) {
+	d << "Hello world!";
+	return d;
+}
+
 int main(int argc, char *argv[])
 {
-	//QApplication a(argc, argv);
-	//Projekcik w;
-	//w.show();
+	QApplication a(argc, argv);
+	Projekt w;
+	w.show();
 	const QDate curr = QDate::currentDate();
-	int qwerty = 2324;
-	qDebug() << "Date:" << curr;
-	qDebug() << qwerty;
-	qDebug() << "sgoksg";
-	int i = curr.day();
-	qDebug() << i;
+	Zwnswn jot;
+	jot << qDebug();
+	qDebug() << "jot" << jot;
 	//return a.exec();
 	return 0;
 }
+
